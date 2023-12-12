0e3aeaff05250e6f60971e749453d0af87861710
NicoA2002 <114366568+NicoA2002@users.noreply.github.com>
2023-12-12 17:19:39 -0500

    measure peak voltage, and area under curve

--- k410t_jesd.srcs/sources_1/imports/Desktop/data_processing_unit.v
+++ k410t_jesd.srcs/sources_1/imports/Desktop/data_processing_unit.v
@@ -1,4 +1,10 @@
 
+//Author: Nicolas Alarcon
+//Last updated: 12/12/2023
+
+//Note: all metrics are in terms of clock cycles, rather than seconds.
+//122.8 MHz clock
+
 //States for Event Handling, Grey Encoding
 `define IDLE 2'b00
 `define EVENT 2'b01
@@ -14,8 +20,8 @@ module data_processing_unit
     input wire [13:0] A0,
     input wire [13:0] A1,
     output reg valid,
-    //output reg [13:0] V_peak,
-    //output reg [31:0] AUC, //area under curve
+    output reg [13:0] V_peak,
+    output reg [31:0] AUC, //area under curve
     output reg [15:0] dt, //width of spike
     output reg [31:0] IPI //Inter-Peak Interval
 );
@@ -26,16 +32,18 @@ reg [1:0] event_state;
 //counter regs
 reg [31:0] counter; //for dt
 //reg [31:0] end_time;
-//reg [31:0] V_sum; //keeps track of AUC
-//reg [15:0] V_max; //keeps track of V_peak
+reg [31:0] V_sum; //keeps track of AUC
+reg [15:0] V_max; //keeps track of V_peak
 
 //internal wires
 wire event_detected;
 wire go_to_idle;
+wire A_max;
 
 //assign outside of sequential logic for speed
 assign go_to_idle = (event_detected && counter>time_max) || (~event_detected && counter<time_min);
 assign event_detected = A0 > V_threshold || A1 > V_threshold;
+assign A_max = (A0>A1) ? A0:A1;
 
 //always block with asynchronous reset
 always@( posedge clk or negedge RESET_N ) begin
@@ -43,47 +51,62 @@ always@( posedge clk or negedge RESET_N ) begin
     if ( RESET_N == 1'b0 )
         begin
         event_state <= `IDLE;
-        AUC <= 32'h0000;
-        //V_peak <= 14'h00;
+        counter <= 32'h0000;
         dt <= 16'h00;
         IPI <= 32'h0000;
-        counter <= 32'h0000;
-        //V_sum <= 32'h0000; 
+        V_sum <= 32'h0000;
+        AUC <= 32'h0000;
+        V_max <= 14'h00;
+        V_peak <= 14'h00; 
         end 
+
     else begin
 
         case (event_state)
         `IDLE: begin //wait to detect event, reset counter
+
             counter <= 32'h0000;
             dt <= 16'h00;
-            AUC <= 32'h0000;
             IPI <= 32'h0000;
+            V_sum <= 32'h0000;
+            AUC <= 32'h0000;
+            V_max <= 14'h00;
+            V_peak <= 14'h00;
+
             if(event_detected == 1'b1) begin
                 event_state <= `EVENT; 
             end
         end
         `EVENT: begin
             counter <= counter + 1;
+
             if (go_to_idle) begin //checks if valid
                 event_state <= `IDLE; //also resets counter
+
             end else if (event_detected && counter<time_max) begin  //evaluate data metrics
                     //FIXME do all computation, valid
+                    V_max <= (V_max>A_max) ? V_max:A_max;
+                    V_sum <= V_sum + A_max;
                     valid <= 1'b1;
                     
             end else if (~event_detected && counter>time_min) begin //event ends
                     event_state <= `WAITING;
                     valid <= 1'b0;
                     dt <= counter;
-                    //V_peak <= V_max;
-                    //AUC <= V_sum;
+                    V_peak <= V_max;
+                    V_max <= 14'h00;
+                    AUC <= V_sum;
+                    V_sum <= 32'h0000;
             end
         end
         `WAITING: begin
-            counter <= counter + 1; //keep incrementign for IPI
+            counter <= counter + 1; //keep incrementing for IPI
+
             if(event_detected == 1'b1 ) begin
                 event_state <= `EVENT; 
                 IPI <= counter; //counts from last event to next event
                 counter <= 32'h0000;
+                
             end else if(counter>time_max+dt) begin //timed out in waiting
                 event_state <= `IDLE;
             end
